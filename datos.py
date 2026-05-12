78d52fbfcd758c258f90dd5a1b178c9234ce326a
Fernando-HD <123045484@upq.edu.mx>
2026-05-12 11:41:06 -0600

    feat: Agregar estructura de datos educativos

---
+++ datos.py
@@ -0,0 +1,4 @@
+reglas = {'titulo': 'Reglas', 'contenido': ['Asistencia 80%'], 'banco_preguntas': []}
+evaluacion = {'titulo': 'Evaluacion', 'contenido': [], 'banco_preguntas': []}
+habilidades = {'titulo': 'Habilidades', 'contenido': [], 'banco_preguntas': []}
+fechas_clave = [{'evento': 'Examen 1', 'fecha': '10 marzo'}]
